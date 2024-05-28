cd81b4fc8b0668f9b5e4245054df905d6e788a9b
13247823 <adty478s@gmail.com>
2024-05-28 18:23:30 +0900

    상세페이지 추가

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('home/', views.home_view, name='home'),
     path('chatbot/', views.chatbot_view, name='chatbot'),
+    path('drug_detail/', views.drug_detail_view, name='drug_detail'),
 ]
--- single_pages/views.py
+++ single_pages/views.py
@@ -1,17 +1,20 @@
 # views.py
 from django.shortcuts import render
-from .models import ChatbotModel
+from .models import ChatbotModel, DrugModel
 
 def home_view(request):
-    return render(request, 'home.html')
+   return render(request, 'home.html')
 
 def chatbot_view(request):
-    if request.method == 'POST':
-        user_input = request.POST.get('user_input')
-        chatbot = ChatbotModel()
-        response = chatbot.generate_response(user_input)
-        context = {'response': response}
-        return render(request, 'chatbot.html', context)
-    else:
-        context = {'response': ''}
-        return render(request, 'chatbot.html', context)
+   if request.method == 'POST':
+       user_input = request.POST.get('user_input')
+       chatbot = ChatbotModel()
+       response = chatbot.generate_response(user_input)
+       context = {'response': response}
+       return render(request, 'chatbot.html', context)
+   else:
+       context = {'response': ''}
+       return render(request, 'chatbot.html', context)
+
+def drug_detail_view(request):
+   return render(request, 'drug_detail.html')
